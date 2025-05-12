6962cc609fd03197092c70cfec62110649af9d9d
Helwa <helwasalameh14@gmail.com>
2025-05-12 12:46:48 -0500

    "Created the Shield file"

---
+++ src/main/java/Shield.java
@@ -0,0 +1,8 @@
+/**
+ * A defensive item that reduces damage taken.
+ */
+public class Shield extends Item {
+    public Shield() {
+        super("Iron Shield", 5, 5); // Balanced bonuses
+    }
+}
