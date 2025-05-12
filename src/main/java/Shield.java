6cf6468ca5c7ee26e437656f71bdf772e20910f8
Helwa <helwasalameh14@gmail.com>
2025-05-12 12:48:11 -0500

    I forgot to add my name and assignment on the top

--- src/main/java/Shield.java
+++ src/main/java/Shield.java
@@ -1,3 +1,9 @@
+//------------------------------------------------------
+// Assignment 2
+// Written by: Helwa Salameh [1961163]
+// For SE 350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
 /**
  * A defensive item that reduces damage taken.
  */
