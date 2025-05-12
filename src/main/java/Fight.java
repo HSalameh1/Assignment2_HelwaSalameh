43a5eb27b329177aed4dc80bc97941aedb5e4e9b
Helwa <helwasalameh14@gmail.com>
2025-05-12 13:49:55 -0500

    "Added the Fight file"

---
+++ src/main/java/Fight.java
@@ -0,0 +1,38 @@
+//------------------------------------------------------
+// Assignment 2
+// Written by: Helwa Salameh [1961163]
+// For SE 350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+/**
+ * Handles combat between player and a monster.
+ */
+public class Fight extends Action {
+    private final Monster monster;
+
+    public Fight(Dungeon dungeon, Monster monster) {
+        super(dungeon);
+        this.monster = monster;
+    }
+
+    @Override
+    public void execute() {
+        Character player = dungeon.getPlayer();
+        int playerAttack = player.attack(monster);
+        int monsterAttack = monster.attack(player);
+
+        monster.takeDamage(playerAttack);
+        player.takeDamage(monsterAttack);
+
+        if (monster.isDefeated()) {
+            System.out.println("Defeated " + monster.getName() + "!");
+        } else {
+            System.out.println("Both combatants took damage!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Fight " + monster.getName();
+    }
+}
